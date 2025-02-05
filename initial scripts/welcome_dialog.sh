b5a80e940e7cf147a4f858823c8b1610f7c0cd5c
Justin Lafleur <138168052+justinendsens@users.noreply.github.com>
2025-02-05 17:17:32 +0100

    Update welcome_dialog.sh

--- initial scripts/welcome_dialog.sh
+++ initial scripts/welcome_dialog.sh
@@ -23,10 +23,10 @@ runAsUser $dialog \
 --blurscreen \
 --ontop \
 --title "Gefeliciteerd met je nieuwe MacBook!" \
---icon "https://github.com/alliander-opensource/dwp-nextgen-macos/blob/main/Baseline/pictures/logos/logo.png?raw=true" \
+--icon "https://dev.azure.com/ENX-WPL/12456404-0439-4ea9-85c2-7f079cb5d4e3/_apis/git/repositories/e5ea4662-6cac-4c40-900d-57018fbf05a6/items?path=/Baseline/pictures/logos/logo.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=0&versionDescriptor%5Bversion%5D=main&resolveLfs=true&%24format=octetStream&api-version=5.0" \
 --message "Hoi $realName,\n\nWe gaan je helpen om alles in te stellen en de juiste programma's te installeren.\n\nAls de installatie klaar is, zie je een knop met **Herstart**. Klik daarop om je MacBook opnieuw op te starten en de installatie af te ronden.\n\n Dit kan even duren, dus heb geduld...." \
 --messagefont "size=14" \
---bannerimage "https://github.com/alliander-opensource/dwp-nextgen-macos/blob/main/Baseline/pictures/logos/banner.png?raw=true" \
+--bannerimage "https://dev.azure.com/ENX-WPL/12456404-0439-4ea9-85c2-7f079cb5d4e3/_apis/git/repositories/e5ea4662-6cac-4c40-900d-57018fbf05a6/items?path=/Baseline/pictures/logos/banner.png&versionDescriptor%5BversionOptions%5D=0&versionDescriptor%5BversionType%5D=2&versionDescriptor%5Bversion%5D=c8562faef3a5aa1990be86e0bd5744c04311528c&resolveLfs=true&%24format=octetStream&api-version=5.0" \
 --button1text "Aan de slag" \
 --titlefont "size=20" \
 --timer "120" \
